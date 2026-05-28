9c7b51000d8322e45d94cc4c44c5376b10730b9b
Almas Parwaiz <almasparwaiz1@gmail.com>
2026-05-28 17:24:24 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="imdb-sentiment-analysis",
+    version="0.1",
+    install_requires=[
+        "streamlit",
+        "tensorflow-cpu==2.11.0",
+    ],
+)
